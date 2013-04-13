968e2d028fa8b597bc209642e8c32378cd26538d
Jared Gohr <mephisto275@gmail.com>
2013-04-13 15:33:35 -0500

    Bb.maxX/Y

--- Bb.cs
+++ Bb.cs
@@ -8,12 +8,19 @@ namespace Pizza
 {
     public static class Bb
     {
+        public static int MaxX;
+        public static int MaxY;
         public static BitArray Fish;
         public static BitArray Wall;
         public static BitArray Trash;
 
         public static void init()
         {
+            Fish = new BitArray(AI.tiles.Length);
+            Wall = new BitArray(AI.tiles.Length);
+            Trash = new BitArray(AI.tiles.Length);
+
+            //BaseAI.fishes.ToList().ForEach(fish =>Fish[fish.X*)
         }
     }
 }
