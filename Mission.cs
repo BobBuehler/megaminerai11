e890d19e74e7f6dd1fec14ea2d9da5676286665b
Austin Gantner <austin.gantner+bitbucket@gmail.com>
2013-04-13 15:17:14 -0500

    Added Mission.cs

---
+++ Mission.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pizza
+{
+    enum Objective
+    {
+        goTo,
+        getTrash,
+        dropTrash,
+        kill
+    };
+
+    class Mission
+    {
+        Fish m_agent;
+        int m_x = 0;
+        int m_y = 0;
+        Objective m_obj = Objective.goTo;
+
+
+        Mission(Fish Agent)
+        {
+            m_agent = Agent;
+
+        }
+
+        Mission(Fish Agent, Objective O)
+        {
+            m_agent = Agent;
+            m_obj = O;
+        }
+
+
+    }
+}
