c34fc66b5a9d60435a2a1ac7666cb37cc0571afc
Jared Gohr <mephisto275@gmail.com>
2013-04-13 16:27:20 -0500

    adding bit maps/sets
    CoveMap and CoveSet objects implimented in Bb

--- Bb.cs
+++ Bb.cs
@@ -10,17 +10,87 @@ namespace Pizza
     {
         public static int MaxX;
         public static int MaxY;
-        public static BitArray Fish;
-        public static BitArray Wall;
-        public static BitArray Trash;
 
-        public static void init()
+        public static HashSet<Tile> CoveSet = new HashSet<Tile>();
+        public static HashSet<Tile> OurCoveSet = new HashSet<Tile>();
+        public static HashSet<Tile> TheirCoveSet = new HashSet<Tile>();
+
+        public static BitArray FishMap;
+        public static BitArray OurFishMap;
+        public static BitArray TheirFishMap;
+        public static BitArray WallMap;
+        public static BitArray TrashMap;
+        public static BitArray OurTrashMap;
+        public static BitArray TheirTrashMap;
+        public static BitArray OurCoveMap;
+        public static BitArray CoveMap;
+        public static BitArray TheirCoveMap;
+
+        public static void init(AI ai)
         {
-            Fish = new BitArray(AI.tiles.Length);
-            Wall = new BitArray(AI.tiles.Length);
-            Trash = new BitArray(AI.tiles.Length);
+            MaxX = ai.mapWidth();
+            MaxY = ai.mapHeight();            
 
-            //BaseAI.fishes.ToList().ForEach(fish =>Fish[fish.X*)
+            FishMap = new BitArray(AI.tiles.Length);
+            OurFishMap = new BitArray(AI.tiles.Length);
+            TheirFishMap = new BitArray(AI.tiles.Length);
+
+            WallMap = new BitArray(AI.tiles.Length);
+
+            TrashMap = new BitArray(AI.tiles.Length);
+            OurTrashMap = new BitArray(AI.tiles.Length);
+            TheirTrashMap = new BitArray(AI.tiles.Length);
+
+            CoveMap = new BitArray(AI.tiles.Length);
+            OurCoveMap = new BitArray(AI.tiles.Length);
+            TheirCoveMap = new BitArray(AI.tiles.Length);
+
+            foreach (var fish in BaseAI.fishes)
+            {
+                FishMap[GetOffset(fish.X, fish.Y)] = true;
+                if (fish.Owner == ai.playerID())
+                {
+                    OurFishMap[GetOffset(fish.X, fish.Y)] = true;
+                }
+                else
+                {
+                    TheirFishMap[GetOffset(fish.X, fish.Y)] = true;
+                }
+            }
+
+            foreach (var tile in BaseAI.tiles)
+            {
+                if (tile.Owner == 3)//is a wall
+                {
+                    WallMap[GetOffset(tile.X, tile.Y)] = true;
+                }
+
+            }
+
+            foreach (var tile in BaseAI.tiles)
+            {
+                if (tile.Owner == 0 || tile.Owner == 1)
+                {
+                    CoveSet.Add(tile);
+                    CoveMap[GetOffset(tile.X, tile.Y)] = true;
+                    
+                    if (tile.Owner == ai.playerID())
+                    {
+                        OurCoveSet.Add(tile);
+                        OurCoveMap[GetOffset(tile.X, tile.Y)] = true;
+                    }
+                    else
+                    {
+                        TheirCoveSet.Add(tile);
+                        TheirCoveMap[GetOffset(tile.X, tile.Y)] = true;
+                    }
+                }
+            }
+        }
+
+        public static int GetOffset(int x, int y)
+        {
+            return (y * MaxX + x);
         }
     }
 }
