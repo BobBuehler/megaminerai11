5de14196062848d6bb38dbbcedabb4f8c9e29fab
Bob <bob.buehler@gmail.com>
2013-04-13 15:21:03 -0500

    Act and Pather

---
+++ Act.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pizza
+{
+    public enum ActType
+    {
+        move,
+        pickup,
+        drop,
+        attack
+    }
+
+    public class Act
+    {
+        public ActType type;
+        public int x;
+        public int y;
+        public Tile tile;
+        public int weight;
+        public Fish target;
+
+        public bool act(Fish fish)
+        {
+            switch (type)
+            {
+                case ActType.move:
+                    return fish.move(x, y);
+                case ActType.pickup:
+                    return fish.pickUp(tile, weight);
+                case ActType.drop:
+                    return fish.drop(tile, weight);
+                case ActType.attack:
+                    return fish.attack(target);
+            }
+            return false;
+        }
+    }
+}
---
+++ Pather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Pizza
+{
+    public static class Pather
+    {
+        public static IEnumerable<Act> calculate(int xStart, int yStart, int xEnd, int yEnd, BitArray passable)
+        {
+            return new Act[] { };
+        }
+    }
+}
