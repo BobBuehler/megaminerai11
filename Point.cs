efcdb98b0a525ee01ef1b61c2e8c8a3972bc3e14
Bob <bob.buehler@gmail.com>
2013-04-13 16:26:09 -0500

    Point

---
+++ Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pizza
+{
+    public struct Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X;
+        public int Y;
+
+        public override bool Equals(object obj)
+        {
+            //if (!(obj is Point))
+            //{
+            //    return false;
+            //}
+            Point o = (Point)obj;
+            return X == o.X && Y == o.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X ^ Y;
+        }
+    }
+}
