db8f13bd44624d5466a5060aa290308e7ce02c83
Bob <bob.buehler@gmail.com>
2013-04-13 20:45:33 -0500

    Executor

---
+++ Executor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pizza
+{
+    class Executor
+    {
+        public static void Execute(AI ai, Mission mission)
+        {
+            switch(mission.m_obj)
+            {
+                case Objective.goTo:
+                    GoTo(ai, mission);
+                    break;
+            }
+            return;
+        }
+
+        public static void GoTo(AI ai, Mission mission)
+        {
+
+        }
+    }
+}
