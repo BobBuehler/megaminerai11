b9181084dc2210f39033c97f9d22d35f923f8ca0
Jared Gohr <mephisto275@gmail.com>
2013-04-14 02:41:14 -0500

    mend

--- Picker.cs
+++ Picker.cs
@@ -12,28 +12,98 @@ namespace Pizza
         {
             List<Fish> targets = new List<Fish>();
 
-
-            foreach (Point p in Bb.BitArrayToList(new BitArray(Bb.FishMap).And(Bb.GetNAwayFromPoint(range, point))))
+            foreach (Fish fish in BaseAI.fishes)
             {
-                foreach (Fish fish in BaseAI.fishes)
+                int manhDist = (Math.Abs(fish.X - point.X) + Math.Abs(fish.Y - point.Y));
+                if(manhDist <= range && fish.Owner == enemyId)
                 {
-                    if (fish.Owner == enemyId && fish.X == p.X && fish.Y == p.Y)
-                    {
-                        targets.Add(fish);
-                    }
+                    targets.Add(fish);
                 }
             }
             return targets;
-
         }
 
-        public static Fish GetBestTarget(List<Fish> targets)
+        public static Fish GetBestTarget(IEnumerable<Fish> targets, Fish attacker)
         {
-            //Fish bestTarget;
+            IComparer<Fish> myComparer = (IComparer<Fish>)new sortTargetsHelper(attacker);
+            List<Fish> targetList = targets.ToList();
+            targetList.Sort(myComparer);
 
-            targets.Sort((x, y) => x.CurrentHealth.CompareTo(y.CurrentHealth));
-
-            return targets[0];
+            return targetList[0];
+            
+            
         }
+
+
+        public class sortTargetsHelper : IComparer<Fish>
+        {
+            Fish attacker;
+            public sortTargetsHelper(Fish a_attacker)
+            {
+                attacker = a_attacker;
+            }
+            
+            int IComparer.Compare(Fish a, Fish b)
+            {
+                if (a.CurrentHealth <= attacker.AttackPower && b.CurrentHealth > attacker.AttackPower)
+                {
+                    return 1;
+                }
+                else if (b.CurrentHealth <= attacker.AttackPower && a.CurrentHealth > attacker.AttackPower)
+                {
+                    return -1;
+                }
+                else //if(a.CurrentHealth <= attacker.AttackPower && b.CurrentHealth <= attacker.AttackPower)
+                {
+                    if (a.Species == (int)AI.SpeciesIndex.SEA_STAR && b.Species != (int)AI.SpeciesIndex.SEA_STAR)
+                    {
+                        return 1;
+                    }
+                    else if (b.Species == (int)AI.SpeciesIndex.SEA_STAR && a.Species != (int)AI.SpeciesIndex.SEA_STAR)
+                    {
+                        return -1;
+                    }
+                    else if (a.Species == (int)AI.SpeciesIndex.SEA_URCHIN && b.Species != (int)AI.SpeciesIndex.SEA_URCHIN)
+                    {
+                        return -1;
+                    }
+                    else if (b.Species == (int)AI.SpeciesIndex.SEA_URCHIN && a.Species != (int)AI.SpeciesIndex.SEA_URCHIN)
+                    {
+                        return 1;
+                    }
+                    else if (a.Species == (int)AI.SpeciesIndex.SEA_URCHIN && b.Species == (int)AI.SpeciesIndex.SEA_URCHIN)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        if (a.CarryingWeight > b.CarryingWeight)
+                        {
+                            return 1;
+                        }
+                        else if (b.CarryingWeight < a.CarryingWeight)
+                        {
+                            return -1;
+                        }
+                        else
+                        {
+                            if (a.CurrentHealth > b.CurrentHealth)
+                            {
+                                return 1;
+                            }
+                            else if (b.CurrentHealth > a.CurrentHealth)
+                            {
+                                return -1;
+                            }
+                            else
+                            {
+                                return 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
